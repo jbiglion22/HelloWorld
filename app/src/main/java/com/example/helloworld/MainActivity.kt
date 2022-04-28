1f28881805a94e1bbb58cfc76ebf6017f03f2a32
JBig <jbiglion22@gmail.com>
2022-04-28 21:12:01 +0900

    2022.4.28 21:11

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 테스트 24 202222 2022.4.28
+        // 테스트 24 202222 2022.4.28 e
 
     }
 }
